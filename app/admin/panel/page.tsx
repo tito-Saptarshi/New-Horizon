50e4749572cef565ac45d0aa3fcd153ee0d7c99a
tito-Saptarshi <saptarshi.tito.20@gmail.com>
2024-12-11 20:08:03 +0530

    error fix v0.22

--- app/admin/panel/page.tsx
+++ app/admin/panel/page.tsx
@@ -1,25 +1,28 @@
-"use client";
+// "use client";
 
 import Link from "next/link";
-import { useSearchParams } from "next/navigation";
+// import { useSearchParams } from "next/navigation";
 
 const NewPage = () => {
-  const searchParams = useSearchParams();
-  const passKey = searchParams.get("passKey");
-  // const pass = process.env.NEXT_PUBLIC_PASS_ADMIN;
-  const pass = "abc"
+  // const searchParams = useSearchParams();
+  // const passKey = searchParams.get("passKey");
+  // // const pass = process.env.NEXT_PUBLIC_PASS_ADMIN;
+  // const pass = "abc"
 
-
-  if (passKey !== pass) {
-    return <div>invalid</div>;
-  }
+  // if (passKey !== pass) {
+  //   return <div>invalid</div>;
+  // }
 
   return (
     <div className="p-10">
       <h1 className="text-center text-2xl">Admin panel</h1>
       <div className="flex flex-col p-3">
-        <Link className="p-3" href={"/admin/dates"}>Create Dates</Link>
-        <Link className="p-3" href={"/admin/students"}>View Students</Link>
+        <Link className="p-3" href={"/admin/dates"}>
+          Create Dates
+        </Link>
+        <Link className="p-3" href={"/admin/students"}>
+          View Students
+        </Link>
       </div>
     </div>
   );
